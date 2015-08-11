8a15c8e6e89a27d33cb885935a469054cced6bbf
Alex B. Jose <alexbjose@gmail.com>
2015-08-10 21:42:14 -0700

    add filtering

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 /* TODO
 - ADD SORTING STUFF THEN STOP AND WORK ON org-to-html part
-- use colons instead of spaces to delimit tags?
-- get css lookin' all nice
+- use colons instead of spaces to delimit tags
 - figure out what things will look like if both text and projects
 - nice if had filters in URL bar so could "deep link"?
 - expose properties / sort stuff by properties
@@ -9,74 +8,99 @@
   https://news.ycombinator.com/item?id=9963714
   https://css-tricks.com/snippets/css/a-guide-to-flexbox/
   https://philipwalton.github.io/solved-by-flexbox/demos/grids/
-
-sorting mini-todo
-- need to grab properties from divs and reorder
-- easiest way to re-order just to use JS?
-- to grab properties - should be pretty straightforward, just iterate over
-  all projects 
-- to sort once have properties...
-  definitely just use array.sort(fnc)
-  should store an array of all the elements globally, use that for determining order
-  then just have the function check the desired value (and use some default value)
-  how to integrate global list? shouldn't need to change hiding/showing code
-  and also should be fine with adding/removing from the dom?
-  maybe there are also functions to shuffle around order...
-- how to do special case for date?
+- have different js (or none) for "content" page (project)
+- try out with JS off
 */
 
-var filters = [];
 initFilters();
-updateProjects("date", false);
-updateFilters();
+initProperties();
+filterProjects();
+orderProjects()
+
+function hide(e) {
+  e.classList.add("hidden");
+  e.classList.remove("visible");
+}
+
+function show(e) {
+  e.classList.add("visible");
+  e.classList.remove("hidden");
+}
+
+function toggleSelected(filter) {
+  filter.classList.toggle("selected");
+}
+
+function parseTags(project) {
+  // Expects tags in format "tag1 tag2", return ["tag1", "tag2"].
+  var tags = project.attributes["data-tags"].value.split(" ");
+  if (tags[0] == "") return [];
+  return tags;
+}
+
+function getTagMap(projects) {
+  var tag_map = {};
+  for (var i = 0; i < projects.length; ++i) {
+    var tags = parseTags(projects[i]);
+    for (var j = 0; j < tags.length; ++j) {
+      tag_map[tags[j]] = true;
+    }
+  }
+
+  return tag_map;
+}
+
+function getFilterContent(filters) {
+  var content = [];
+  for (var i = 0; i < filters.length; ++i) {
+    content.push(filters[i].innerHTML);
+  }
+  return content;
+}
 
 function initFilters() {
-  var filters = document.getElementsByClassName("filter");
-  for (var i = 0; i < filters.length; ++i) {
-    var filter = filters[i];
+  // for each project, build up object of tags
+  // for each tag, add a filter
+  // attach on-click
+  var projects = document.getElementsByClassName("project");
+  var tag_map = getTagMap(projects);
+  var tags = Object.keys(tag_map);
+
+  var filters = document.getElementById("filters");
+  for (var i = 0; i < tags.length; ++i) {
+    var filter = document.createElement("div");
+    filter.innerHTML = tags[i];
+    filter.setAttribute("class", "filter");
+    show(filter);
     filter.onclick = function() {
-      toggleFilter(this);
+      toggleSelected(this);
+      filterProjects();
     }
+
+    filters.appendChild(filter);
   }
 }
 
-function toggleFilter(filter) {
-  var index = filters.indexOf(filter.innerHTML);
-  if (index !== -1) {
-    filter.style.fontWeight = "";
-    filters.splice(index, 1);
-  } else {
-    filter.style.fontWeight = "bold";
-    filters.push(filter.innerHTML);
-  }
-  updateFilters();
-  updateProjects("date", false);
-}
-
-function onPropertyChange() {
-}
-
-function onSortOrderChange() {
-}
-
-function updateFilters() {
-  // Only show "possible" filters.
-  // TODO: don't repeat yourself.
-
-  var visible_filters = {};
-  // Accumulate visible filters.
+function filterProjects() {
+  // get all selected filters
+  // for each project, for each selected filter,
+  // if doesn't have one, hide
+  var selected_filters = document.getElementsByClassName("selected filter");
+  selected_filters = getFilterContent(selected_filters);
   var projects = document.getElementsByClassName("project");
   for (var i = 0; i < projects.length; ++i) {
-    var project = projects[i];    
-    var tags = parseTags(project);
-    var is_visible = true;
-
-    for (var j = 0; j < filters.length; ++j) {
-      if (tags.indexOf(filters[j]) == -1) {
-	is_visible = false;
-	break;
+    var project_tags = parseTags(projects[i]);
+    var should_show = true;
+    if (selected_filters.length > 0) {
+      for (var j = 0; j < selected_filters.length; ++j) {
+        if (project_tags.indexOf(selected_filters[j]) == -1) {
+          should_show = false;
+          break;
+        }
       }
     }
+    should_show ? show(projects[i]) : hide(projects[i]);
+  }
 
     if (is_visible) {
       for (var j = 0; j < tags.length; ++j) {
